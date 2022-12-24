9d8b4f0afd92bab57df959975f47d07bc2fb5169
Consulting Ninja <116985458+consultingninja@users.noreply.github.com>
2022-12-24 13:56:36 -0600

    Delete app directory
    remove...

--- app/immutable/chunks/0-cc7b90ee.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/layout.svelte-3758a141.js";export{t as component};
--- app/immutable/chunks/1-29df6a16.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/error.svelte-258554f4.js";export{t as component};

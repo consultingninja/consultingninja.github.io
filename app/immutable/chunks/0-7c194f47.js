60681785d14c7f8dc57f82c554342a4651c332bf
Consulting Ninja <116985458+consultingninja@users.noreply.github.com>
2022-12-24 15:01:27 -0600

    Add files via upload
    try again to reconfig svelte

---
+++ app/immutable/chunks/0-7c194f47.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/layout.svelte-2abcfb3c.js";export{t as component};
---
+++ app/immutable/chunks/1-86aee688.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/error.svelte-868e2f19.js";export{t as component};

def23e4bee16d9310b389a9f170e6084f4507833
Consulting Ninja <116985458+consultingninja@users.noreply.github.com>
2023-01-20 16:56:06 -0600

    Add files via upload
    fix api to use consultingninja

---
+++ _app/immutable/chunks/0-dd2853e2.js
@@ -0,0 +1,1 @@
+import{_ as r}from"./_layout-933959ff.js";import{default as t}from"../components/pages/_layout.svelte-9e0da3ea.js";export{t as component,r as universal};
---
+++ _app/immutable/modules/pages/_layout.js-6f720dd7.js
@@ -0,0 +1,1 @@
+import{l}from"../../chunks/_layout-933959ff.js";export{l as load};
